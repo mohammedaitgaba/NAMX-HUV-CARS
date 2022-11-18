7b5bed9e52cd685c8af6606ea5abbccc2c5ab298
Halazv2 <halaziani2003@gmail.com>
2022-11-18 17:53:41 +0100

    app is middleware that is used to handle requests in our express app

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const globalErrHandler = require("./src/controllers/errorController");
+const AppError = require("./src/utils/appError");
+const app = express();
+
+// Allow Cross-Origin requests
+app.use(cors());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit request from the same API
+const limiter = rateLimit({
+  max: 150,
+  windowMs: 60 * 60 * 1000,
+  message: {
+    status: "fail",
+    message: "Too many requests from this IP, please try again in an hour!",
+  },
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: "15kb",
+  })
+);
+
+// Data sanitization against Nosql query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS(clean user input from malicious HTML code)
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
+
+/* Parsing the body of the request. */
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+
+require("./src/routes/index.routes")(app, "/api/v1/mekna7");
+
+// // handle undefined Routes
+app.use("*", (req, res, next) => {
+  const err = new AppError(404, "fail", "undefined route");
+  next(err, req, res, next);
+});
+
+app.use(globalErrHandler);
+
+module.exports = app;
