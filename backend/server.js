80c59c1228eb42968856e290943d5dada617fe54
Halazv2 <halaziani2003@gmail.com>
2022-11-20 14:55:10 +0100

    where app start

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const app = require('./app');
+
+require("dotenv").config();
+const connectDB = require("./src/config/db.config");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION!!! shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+connectDB();
+
+
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION!!!  shutting down ...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
