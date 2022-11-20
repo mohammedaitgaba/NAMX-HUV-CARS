72ecb898df6c3384ebc3f2235750fdec5519401c
Halazv2 <halaziani2003@gmail.com>
2022-11-20 14:58:58 +0100

    routes

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const { verifySignUp } = require("../middlewares");
+const controller = require("../controllers/auth.controller");
+
+module.exports = function (app, url) {
+
+  app.post(
+    `${url}/signup`,
+    [verifySignUp.checkDuplicateEmail, verifySignUp.checkRolesExisted],
+    controller.signup
+  );
+  app.post(`${url}/signin`, controller.signin);
+  app.post(`${url}/verifyisAdmin`, controller.verifyisAdmin);
+};
---
+++ backend/src/routes/index.routes.js
@@ -0,0 +1,3 @@
+module.exports = function (app, url) {
+  require("./auth.routes")(app, url);
+};
