2cc420f375883d69807e5f20959d6861828c5ace
Halazv2 <halaziani2003@gmail.com>
2022-11-22 20:07:06 +0100

    Update Profile

---
+++ backend/src/controllers/Client/ProfileController/index.js
@@ -0,0 +1,20 @@
+"use strict";
+const db = require("../../../models/auth");
+const AppError = require("../../../middlewares/appErrorMiddleware");
+const User = db.user;
+const bcrypt = require("bcryptjs");
+
+exports.UpdateProfile = (req, res) => {
+  User.updateOne(
+    { _id: req.body._id },
+    {
+      full_name: req.body.full_name,
+      password: bcrypt.hashSync(req.body.password, 8),
+    },
+    (err, user) => {
+      err
+        ? AppError(err)
+        : res.send({ message: "User was updated successfully." });
+    }
+  );
+};
