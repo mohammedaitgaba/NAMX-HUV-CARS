69e8bfed2e6ae8f94430c14eb212266157bab45e
Halazv2 <halaziani2003@gmail.com>
2022-11-24 09:36:11 +0100

    Update Profile router

---
+++ backend/src/routes/Profile.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { authJwt } = require("../middlewares");
+const controller = require("../controllers/Client/ProfileController");
+
+module.exports = function (app, url) {
+  app.use(url, router);
+  router.put("/user", [authJwt.verifyToken], controller.UpdateProfile);
+};
