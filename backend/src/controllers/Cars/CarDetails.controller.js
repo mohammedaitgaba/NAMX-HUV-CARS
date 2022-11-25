602e94d92e4e1992dba3169b6c259bd4cdee487c
Halazv2 <halaziani2003@gmail.com>
2022-11-25 09:51:45 +0100

    NHC-24 Cleaning folder structure -> Models

--- backend/src/controllers/Cars/CarDetails.controller.js
+++ backend/src/controllers/Cars/CarDetails.controller.js
@@ -1,12 +1,12 @@
-const CarDetails = require("../../models/CarDetails.model");
+const CarDetails = require("../../models/Car/CarDetails.model");
 
 const getAllCarDetails = async (req, res) => {
-  const Details = await CarDetails.find({}).populate('Car');
+  const Details = await CarDetails.find({}).populate("Car");
   res.json({ Details });
 };
 
 const setCarDetails = async (req, res) => {
-  const { Car,Key, Value } = req.body;
+  const { Car, Key, Value } = req.body;
   if (Object.values(req.body).some((v) => !v)) {
     return res.sendStatus(401, {
       message: "Please fill all required fields",
@@ -25,45 +25,49 @@ const setCarDetails = async (req, res) => {
 };
 
 const UpdateCarDetail = async (req, res) => {
+  const Details = await CarDetails.findById(req.params.id);
 
-  const Details = await CarDetails.findById(req.params.id)
-  
-  if(!Details) {
-    res.status(401).send({message: "Invalide Params" })
-  };
-  
-  if(Object.values(req.body).some((e) => !e)) {
+  if (!Details) {
+    res.status(401).send({ message: "Invalide Params" });
+  }
+
+  if (Object.values(req.body).some((e) => !e)) {
     return res.sendStatus(401, {
       message: "Please fill all required fields",
     });
   }
 
-  const UpdateDetail = await CarDetails.findByIdAndUpdate(req.params.id, req.body, {
-    new: true
-  })
+  const UpdateDetail = await CarDetails.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
 
   res.send(UpdateDetail);
   console.log(UpdateDetail);
 
-  res.status(201).json(UpdateDetail)
-}
+  res.status(201).json(UpdateDetail);
+};
 
 const DeleteCarDetail = async (req, res) => {
+  const Delete = await CarDetails.findById(req.params.id);
 
-  const Delete = await CarDetails.findById(req.params.id)
-
-  if(!Delete) {
-    res.status(401).send({ message: "Invalide Params" })
+  if (!Delete) {
+    res.status(401).send({ message: "Invalide Params" });
   }
 
-  const Deleted = await CarDetails.findByIdAndUpdate(req.params.id, { $set: { isDeleted: true }})
+  const Deleted = await CarDetails.findByIdAndUpdate(req.params.id, {
+    $set: { isDeleted: true },
+  });
 
-  res.status(201).json(Deleted)
-}
+  res.status(201).json(Deleted);
+};
 
 module.exports = {
   getAllCarDetails,
   setCarDetails,
   UpdateCarDetail,
-  DeleteCarDetail
+  DeleteCarDetail,
 };
