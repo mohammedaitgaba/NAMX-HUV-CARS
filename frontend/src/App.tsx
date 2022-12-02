b837610a7ce231a85967a97d17560f3342746cdf
mohammedaitgaba <medgaba237@gmail.com>
2022-12-02 10:21:49 +0100

    add Cars_Pub componnent to home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ContactUs from "./pages/ContactUs";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home/>} />
           <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
       </BrowserRouter>
