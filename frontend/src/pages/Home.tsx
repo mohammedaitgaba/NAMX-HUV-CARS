3e25334fdc4ba0890db8fbb60915f52854aba90a
mohammedaitgaba <medgaba237@gmail.com>
2022-12-04 15:56:09 +0100

    add NamxCarBanner to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import CarsPub from '../components/homeComponnents/CarsPub'
 import Banner from '../components/homeComponnents/Banner'
 import NamxInfo from '../components/homeComponnents/NamxInfo'
+import NamxCarBanner from '../components/homeComponnents/NamxCarBanner'
 const Home=()=>{
   return (
     <>
       <Banner/>
       <CarsPub/>
       <NamxInfo/>
+      <NamxCarBanner/>
     </>
   )
 }
