9788d2e619558977357bd3d4ce737b8bc8bc5d67
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-12-09 12:33:06 +0100

    added rotation in the model 3d

--- frontend/src/pages/anumation/IndexModel.tsx
+++ frontend/src/pages/anumation/IndexModel.tsx
@@ -10,27 +10,27 @@ import { Car } from "../../pages/anumation/Car";
 const IndexModel = () => {
     return (
         <div className='h-screen'>
-            <Canvas className="flex justify-center">
+            {/* <Canvas className="flex justify-center">
                 <OrbitControls enableZoom={false} />
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[-2, 5, 2]} intensity={1} />
                 <Suspense fallback={null}>
                     <Box />
                 </Suspense>
-            </Canvas>
+            </Canvas> */}
 
-            <Canvas className="flex justify-center">
+            {/* <Canvas className="flex justify-center">
                 <OrbitControls enableZoom={false} />
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[-2, 5, 2]} intensity={1} />
                 <Suspense fallback={null}>
                     <SphereAnimated />
                 </Suspense>
-            </Canvas>
+            </Canvas> */}
             <Canvas className="flex justify-center">
                 <OrbitControls enableZoom={false} />
                 <ambientLight intensity={0.5} />
-                <directionalLight position={[-2, 5, 2]} intensity={1} />
+                <directionalLight position={[10, 10, 5]} intensity={1} />
                 <Suspense fallback={null}>
                     <Car />
                 </Suspense>
